e71269c44a78c80638ced61a7119f4bc72492629
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-12-28 03:27:29 +0530

    Redux toolkit added to project

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
+
+        <form action="">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <button type="submit">Login</button>
+        </form>
       
     </div>
   )
