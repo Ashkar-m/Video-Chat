a2885880379e517e2ca804f85704ac9f8eb74c6f
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-12-29 07:52:59 +0530

    initial setup for Login page done

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function getUser(){
+    let user = localStorage.getItem('user');
+    if(user){
+        user = JSON.parse(user);
+    } else{
+        user = null;
+    }
+    return user;
+}
 
 const Home = () => {
+    const [user, setUser] = useState(getUser());
+
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        setUser(null);
+    }
   return (
     <div>
-        <Link to='/login'>Login</Link>
+        {user?(
+            <>
+            <h4>Hello, {user.firstName} {user.lastName}</h4>
+            <h5>{user.email}</h5>
+            <button onClick={handleLogout()}>Logout</button>
+            </>
+        ):(
+            <Link to='/login'>Login</Link>
+        )}  
     </div>
   )
 }
--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../store/UserSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {loading, error} = useSelector((state)=> state.user);
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        let userCredentials = {
+            email, password
+        }
+        dispatch(loginUser(userCredentials)).then((result)=>{
+            if(result.payload){
+                setEmail('');
+                setPassword('');
+                navigate('/');
+            }
+        })
+    }
   return (
     <div>
 
-        <form action="">
+        <form action="" onSubmit={handleLogin}>
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
+            <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit">Login</button>
+            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button type="submit">
+                {loading?'Loading':'Login'}
+            </button>
+            {error&&(
+                <div>{error}</div>
+            )}
         </form>
       
     </div>
