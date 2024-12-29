e5dfc99dc99495c7114542bfcef2f56e7b9f632e
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-12-29 12:49:35 +0530

    Permission in the custom model is added

--- BackEnd/customer/models.py
+++ BackEnd/customer/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
 from django.db import models
 from django.utils import timezone 
 
@@ -12,9 +12,15 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
     def create_superuser(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
 
         return self.create_user(email, username, password, **extra_fields)
 
@@ -33,6 +39,12 @@ class CustomUser(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
 
     def __str__(self):
         return self.email
